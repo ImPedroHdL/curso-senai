d8ef4fcee11998805965500ce418e8d16e239add
Pedro <86620781+ImPedroHdL@users.noreply.github.com>
2024-04-08 21:34:59 -0300

    adicionado exercicios de while e innerHTML

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let resultado = document.getElementById('divSaida')
+console.log(resultado)
+
+//let nome = prompt("Qual seu nome? ")
+
+//resultado.innerHTML = `ola usuario`
+
+
+// let cont = 0
+// while(cont < 10){
+
+//     resultado.innerHTML+=(`${cont} patinhos foram passear<br>`)
+//     cont++
+// }
+
+
+let numeroQuadrado = document.getElementById('numeroQuadrado')
+
+numeroQuadrado.innerHTML
