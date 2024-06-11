d311cc1252164b783ea01e2cfa85d7a0e09622fb
ImPedroHdL <contato-pedrohl@outlook.com>
2024-06-10 21:38:58 -0300

    att de formData e js

--- formDatas/exercicios/exe1.js
+++ formDatas/exercicios/exe1.js
@@ -6,9 +6,51 @@ function avaliar(evento) {
     const profissao = document.querySelector('#user_profission').value
     const fotoUsuario = document.querySelector('#user_pic').value
     const foto = document.querySelector('img')
-    const resultado = document.querySelector('#imcText')
+    let resultado = document.querySelector('#imcText')
+
+    const resultadoNome = document.querySelector('#txtNome')
+    const resultadoPeso = document.querySelector('#txtPeso')
+    const resultadoAltura = document.querySelector('#txtAltura')
+    const resultadoProfissao = document.querySelector('#txtProfissao')
+
+
+
+
     let resultadoIMC = peso / (altura * altura)
 
-    resultado.textContent = `Nome: ${nome}\nPeso: ${peso}\nAltura: ${altura}\nProfissão: ${profissao}\nIMC: ${resultadoIMC.toFixed(2)}`
-    foto.setAttribute("src", fotoUsuario)
+
+    switch (resultadoIMC) {
+
+        case (resultadoIMC < 18.5):
+            resultado = `O seu IMC é ${resultadoIMC}, que é considerado "Magreza"`
+            break
+
+        case (resultadoIMC > 18.5 && resultadoIMC < 24.9):
+            resultado = `O seu IMC é ${resultadoIMC}, que é considerado "Normal"`
+            break
+
+        case (resultadoIMC > 25 && resultadoIMC < 29.9):
+            resultado = `O seu IMC é ${resultadoIMC}, que é considerado "Sobrepeso"`
+            break
+
+        case (resultadoIMC > 30 && resultadoIMC < 39.9):
+            resultado = `O seu IMC é ${resultadoIMC}, que é considerado "Obesidade"`
+            break
+
+        case (resultadoIMC > 40):
+            resultado = `O seu IMC é ${resultadoIMC}, que é considerado "Obesidade Grave"`
+            break
+
+        default:
+            resultado = `Resultado inválido`
+
+    }
+
+    resultadoNome.textContent = `Nome: ${nome}`
+    resultadoPeso.textContent = `Peso: ${peso}`
+    resultadoAltura.textContent = `Altura: ${altura}`
+    resultadoProfissao.textContent = `Profissão: ${profissao}`
+    resultado.textContent = (resultado)
+
+    foto.setAttribute = ("src", fotoUsuario)
 }
