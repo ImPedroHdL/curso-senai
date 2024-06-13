9fcb94288164845a480966f51266156cd3a40bcc
ImPedroHdL <contato-pedrohl@outlook.com>
2024-06-12 22:25:12 -0300

    adc exemplos e exes de pseudoClasses

---
+++ homeMades/treino1.js
@@ -0,0 +1,33 @@
+function somar() {
+    // elemento importado
+    let soma = document.querySelector('.soma')
+
+    // manipulando a classe da div
+    soma.classList.toggle('oculto')
+
+}
+
+// function subtrair() {
+//     // elemento importado
+//     let soma = document.querySelector('#soma')
+
+//     // manipulando a classe da div
+//     soma.classList.toogle('subtrair')
+// }
+
+// function multiplicar() {
+
+//     // elemento importado
+//     let soma = document.querySelector('#multiplicar')
+
+//     // manipulando a classe da div
+//     soma.classList.toogle('multiplicar')
+// }
+
+// function dividir() {
+//     // elemento importado
+//     let soma = document.querySelector('#dividir')
+
+//     // manipulando a classe da div
+//     soma.classList.toogle('dividir')
+// }
